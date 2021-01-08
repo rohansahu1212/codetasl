f9947c456c25c20e7794b834ccba7b94dae26d63
rohansahu1212 <25528985+rohansahu1212@users.noreply.github.com>
2021-01-08 22:15:10 +0530

    Add files via upload

---
+++ ftch.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+const express = require('express');
+
+var port = 3000;
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');  //open html file
+    
+});
+
+
+var server=app.listen(port, function () {
+    console.log('Server running at http://localhost:%s', port);
+});
